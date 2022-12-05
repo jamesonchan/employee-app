7f677aca03e572577486c2b47d7ab8e7b54483fa
Jameson Chen <[jamesonychan@gmail.com]>
2022-12-04 23:45:41 -0500

    * Created Employee CRUD backend application

---
+++ src/main/java/group/projects/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package group.projects.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/group/projects/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package group.projects.employeemanager.repo;
+
+import group.projects.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
